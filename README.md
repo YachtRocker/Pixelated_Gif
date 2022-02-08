906ca9fc9a5c5a354fb467222ee44dbbd8c4ed77
YachtRocker <douglasjkohn@gmail.com>
2022-02-08 11:43:22 -0600

    Added comments, changed the length of time the images change, added the original image to the gif.  Updated the Readme file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pixelated_Gif
 Create image that goes from normal to pixelated.
+
+This code will take a image, pixelate it into different image with different levels of pixelation and then create a Gif from the images.
--- pix_image.py
+++ pix_image.py
@@ -7,16 +7,22 @@ img = Image.open(img)
 size = 128
 num = 1
 
+img.save('./results/image_0.png')
+
 while (size > 7):
+    # Resizes down to the resampled size
     imgSmall = img.resize((size, size), resample=Image.BILINEAR)
+    # Scale back up to original size
     result = imgSmall.resize(img.size,Image.NEAREST)
+    # Saves Result
     result.save('./results/image_' + str(num) + '.png')
+    # Decreases the size of the image each time through the loop
     size = size - 8
     num += 1
 
 
 
-
+# Creates the GIF
 #filepaths
 fp_in = "./results/image_*.png"
 fp_out = "./results/image.gif"
@@ -24,4 +30,4 @@ fp_out = "./results/image.gif"
 # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
 img.save(fp=fp_out, format='GIF', append_images=imgs,
-         save_all=True, duration=1000, loop=0)
+         save_all=True, duration=600, loop=0)
